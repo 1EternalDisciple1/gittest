ae33d53230f45ca1ddbca89b2d33823b8efe86b1
1EternalDisciple1 <babraev98@mail.ru>
2020-09-27 15:03:49 +0500

    cms

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '631cbad685d302f6abb72f880fa2ccfe'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'bafe4269-f701-4482-b539-db03a6f64d1d'
+};
